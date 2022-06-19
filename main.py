116c88122aa2ea01f5e9bd4f73c784debe3aa958
Daniel Potter <daniel.potter@socialsecurity.gov.scot>
2022-06-19 17:47:00 +0100

    Updated - refactored etc.

--- main.py
+++ main.py
@@ -24,98 +24,78 @@ MENU = {
     }
 }
 
+profit = 0
+
 resources = {
     "water": 300,
     "milk": 200,
     "coffee": 100,
 }
 
+
 def report():
-    for k, v in resources.items():
-        print(f"{k}: {v}")
+    # TODO report
+    print(f"Money taken: {profit}\n{resources}")
 
-def check_resources(drink: str):
 
-    resources_after = {}
 
-    for ingredient, amount in MENU[drink]["ingredients"].items():
-        remaining_amount = resources[ingredient] - amount
-        #print(k)
-        resources_after[ingredient] = remaining_amount
+def check_resources(choice):
+    for key, amount_required in MENU[choice]["ingredients"].items():
+        if amount_required >= resources[key]:
+            print(f"Sorry there is not enough {key}.")
+            return False
 
-        if remaining_amount < 0:
-            print(f"Sorry there is not enough {ingredient}.")
-            return {}
+    return True
 
-    resources_after["money"] = 0
+def process_coins():
+    total = int(input("How many quarters .. "))*0.25
+    total += int(input("How many nickels .. "))*0.1
+    total += int(input("How many dimes .. "))*0.05
+    total += int(input("How many pennies .. "))*0.01
 
-    return resources_after
+    return total
 
-def check_money(amount_paid: dict, drink: str):
+def check_money(cost, coins):
 
-    total_amount = amount_paid["quarters"]*0.25 + amount_paid["dimes"]*0.1 \
-        + amount_paid["nickels"]*0.05 + amount_paid["pennies"]*0.01
+    if cost > coins:
+        print("Sorry that's not enough money. Money refunded.")
+        status = False
+    elif cost < coins:
+        print(f"Here is ${coins - cost} dollars in change.")
+        status = True
+    else:
+        status = True
 
-    return MENU[drink]["cost"] - total_amount
+    return status
 
-def vend(resources_after: object):
+def vend(choice):
+    print("Vending .... ")
+    global profit
     global resources
-    resources = resources_after
-    print(f"mid-vend {resources}: resources")
+    profit += MENU[choice]["cost"]
+    for item, qty in MENU[choice]["ingredients"].items():
+        resources[item] -= qty
 
 def main():
 
-    choice = input("Enter Choice: ").lower()
+    choice = input("What would you like? (espresso/latte/cappuccino):")
 
-    if choice.lower() == "report":
+    if choice.lower() == "off":
+        exit()
+    elif choice.lower() == "report":
         report()
         return
-    elif choice == "off":
-        exit(0)
 
-    # TODO Simplify  and check only 1 of 3 correct choices supplied key exists in dict
-    if choice in MENU.keys():
-
-        resources_after = check_resources(choice)
-
-        if resources_after:
-
-            # TODO check money
-            print("Please insert coins")
-            try:
-                money = {}
-                # TODO check when amount has been reached?
-                money["quarters"] = int(input("quarters = "))
-                money["dimes"] = int(input("dimes = "))
-                money["nickels"] = int(input("nickels = "))
-                money["pennies"] = int(input("pennies = "))
-
-                under_over_amt = check_money(money, choice)
-
-                if under_over_amt > 0:
-                    #Change is due
-                    print("Sorry that's not enough money. Money refunded.")
-                else:
-                    print(f"Here is ${abs(under_over_amt)} change")
-                    resources_after["money"] += MENU[choice]["cost"]
-                    print("Vending ....")
-                    print(f"Enjoy your {choice}!")
-                    vend(resources_after)
-                    print(f"post-vend {resources}: resources")
-
-            except ValueError:
-                print("Invalid coins ....")
-                exit(1)
-
-        else:
-            print("Error - Please report to facilities")
-
+    if choice in MENU:
+        print(choice)
+        if check_resources(choice):
+            total = process_coins()
+            if check_money(MENU[choice]["cost"], total):
+                vend(choice)
     else:
-        print("Please make another choice ")
-        # TODO Offer a new choice or quit
+        print("Please choose a drink from the list")
+        return
 
 if __name__ == "__main__":
-
     while True:
         main()
-        print(resources)
